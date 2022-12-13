bf3992debcfe82c0b52bca3b769bd40835051165
fusellifrancesco <fusellifrancesco05@gmail.com>
2022-12-13 15:13:28 +0100

    Creata classe ProgrammaEventi

---
+++ GestoreEventi/ProgrammaEventi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestoreEventi {
+    public class ProgrammaEventi {
+        private string titolo;
+        private List<Evento> eventi;
+
+        public ProgrammaEventi(string titoloProgramma){
+            this.titolo = titoloProgramma;
+            this.eventi = new List<Evento>();
+        }
+
+        // GETTERS
+
+        public string GetTitolo() {
+            return titolo;
+        }
+
+        // METODI PUBBLICI
+
+        public void AggiungiALista(Evento evento) {
+            this.eventi.Add(evento);
+        }
+
+        //public void StampaEventiPerData(string data) {
+        //    DateTime dataInserita = DateTime.Parse(data);
+        //}
+
+        public static void RappresentaInStringa() {
+            foreach (Evento evento in eventi) {
+                Console.WriteLine(evento.ToString());
+            }
+        }
+
+        public void StampaNumeroEventi() {
+            Console.WriteLine("nella lista sono presenti: " + this.eventi.Count() + " eventi");
+        }
+
+        public void SvuotaLista() {
+            this.eventi.Clear();
+            Console.WriteLine("Hai eliminato tutti gli eventi nella lista");
+        }
+
+    }
+}
